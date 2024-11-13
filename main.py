2c5a77709c86ca528699d2dd77de215e38459970
divyeshsonu <44664822+divyeshsonu@users.noreply.github.com>
2024-11-13 12:55:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6133434192:AAHDkrhW9sgqzLU3ay4o7GRy_yPyDuWJdIc",
-             api_id= 27495136,
-             api_hash= "4ccc4865eec4d8fde7530e71948b3424")
+             bot_token= "7924605822:AAFc7To_ndjnk47DTPZKRu4apXTGYQiXwks",
+             api_id= 28187462,
+             api_hash= "0159fbade6b803a808fbc5e248d52b87")
 
 
 @bot.on_message(filters.command(["start"]))
